919bc5b70c4ba8f39b9e61490c32f648bea9e3a1
larryz1230 <larryzhi1230@gmail.com>
2021-12-04 16:27:49 -0800

    fix grabber subsystem

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/GrabberSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/GrabberSubsystem.java
@@ -1,40 +1,31 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
 import com.acmerobotics.dashboard.config.Config;
+import com.arcrobotics.ftclib.command.SubsystemBase;
+import com.arcrobotics.ftclib.hardware.SimpleServo;
 import com.qualcomm.robotcore.hardware.Servo;
 
 import static java.lang.Thread.sleep;
 
 @Config
-public class GrabberSubsystem {
-    private final Servo grabber;
-    private final Servo arm;
+public class GrabberSubsystem  extends SubsystemBase {
+    private final SimpleServo grabber;
 
-    public static double grabberGrab = 0;
-    public static double grabberRelease = 1;
-    public static double armUp = 0.3;
-    public static double armDown = 0.75;
+    public static double defaultt = 0;
+    public static double drop = -90;
 
-    public GrabberSubsystem(Servo grabber, Servo arm) {
+    public GrabberSubsystem(SimpleServo grabber) {
         this.grabber = grabber;
-        this.arm = arm;
     }
 
-    public void grab() throws InterruptedException {
-        arm.setPosition(armDown);
-        sleep(500);
-        grabber.setPosition(grabberGrab);
+    public void grab()  {
+//        sleep(500);
+        grabber.setPosition(defaultt);
     }
 
-    public void release() throws InterruptedException {
-        arm.setPosition(armDown);
-        sleep(500);
-        grabber.setPosition(grabberRelease);
+    public void release()  {
+//        sleep(500);
+        grabber.setPosition(drop);
     }
 
-    public void armUp() throws InterruptedException {
-        arm.setPosition(armUp);
-        sleep(500);
-        grabber.setPosition(grabberGrab);
-    }
 }
